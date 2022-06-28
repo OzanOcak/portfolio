0f33127fad83ef27ca956e02995cf10a50b52701
OzanOcak <ozanocak@gmail.com>
2022-06-28 10:43:17 -0400

    header-2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import HeaderOption from "../header-option/HeaderOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 function Header() {
   return (
     <div className="header">
@@ -17,6 +20,13 @@ function Header() {
       <div className="header_right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://images.goodsmile.info/cgm/images/product/20210623/11386/86227/large/55494f2f5a79d1d7cadaf7b5912db927.jpg"
+          title="me"
+        />
       </div>
     </div>
   );
